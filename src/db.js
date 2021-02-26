7262f6cc574553013f322674330789078c359e31
Amit <29959138+maverickamit@users.noreply.github.com>
2021-02-26 20:22:06 +0530

    feat: store parsed data in indexedDB

--- src/db.js
+++ src/db.js
@@ -7,5 +7,38 @@ export default function Database() {
   db.version(1).stores({
     files: "++id,name",
   });
+  //Update dbversion to wipe and rewrite the database
+  let dbversion = "version 1";
+  Papa.parse("test.csv", {
+    header: true,
+    download: true,
+    complete: async function (results, file) {
+      try {
+        const value = localStorage.getItem("dbversion");
+        if (value !== dbversion) {
+          db.files
+            .clear()
+            .then(() => {
+              console.log("Database successfully deleted");
+            })
+            .catch((err) => {
+              console.error("Could not delete database");
+            })
+            .finally(() => {
+              results.data.map(async (item) => {
+                await db.files.add({
+                  name: item.name,
+                  item,
+                });
+                localStorage.setItem("dbversion", dbversion);
+              });
+            });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+
   return null;
 }
