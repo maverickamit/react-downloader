5f097ace0dc6bdbe43e9027fe62c0764b4071bb2
Amit <29959138+maverickamit@users.noreply.github.com>
2021-03-08 18:21:06 +0530

    feat: implement function to convert bytes into readable form

--- src/searchResultsTable.jsx
+++ src/searchResultsTable.jsx
@@ -2,6 +2,18 @@ import { DataGrid, RowsProp, ColDef } from "@material-ui/data-grid";
 import Dexie from "dexie";
 import { observer } from "mobx-react-lite";
 
+//function to format bytes into human readablen form
+const formatBytes = (params) => {
+  if (params.value === 0) return "0 Bytes";
+  const k = 1024;
+  const dm = 1;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(params.value) / Math.log(k));
+  return (
+    parseFloat((params.value / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i]
+  );
+};
+
 const SearchResultsTable = observer(({ appStore }) => {
   const columns = [
     {
@@ -13,6 +25,7 @@ const SearchResultsTable = observer(({ appStore }) => {
       headerName: "Size",
       field: "size_bytes",
       flex: 1,
+      valueFormatter: formatBytes,
     },
     {
       headerName: "Seeds",
